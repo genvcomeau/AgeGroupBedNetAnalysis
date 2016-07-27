d6f78de9b8723eda4c6bbf6b204771d8bf521157
genvcomeau <genvcomeau@gmail.com>
2016-07-27 12:23:24 -0700

    7/27 update 1
    yep

--- AgeGroupMasterScript.R
+++ AgeGroupMasterScript.R
@@ -17,3 +17,10 @@ NetuseHHLow$HHtot<-
   NetuseHHLow$`(sum(LNNET))` + NetuseHHLow$`(sum(NoNet))`
 #New estimate of HHsize, takes greater of two methods
 HHNetsLow$HHMNo<- pmax(HHNetsLow$HHMNo, HHNetsLow$HHtot)
+HHNetsLow$pLNNET<- 
+  (HHNetsLow$`(sum(LNNET))`/(HHNetsLow$`(sum(LNNET))`+HHNetsLow$`(sum(NoNet))`))
+Bed.Net.High[c("HHMNo", "NETNO", "Netspp", "pLNNET")]<- NA
+#IMPORTANT HHMNo, NETNO in by land data != OG, rep total
+mlm.Hi<- 
+  merge(Bed.Net.High, HHNetsHi, by.x= "HHNo", by.y= "HHNo")
+mlm.Lo$MAL12<- sub("N", 0, mlm.Lo$MAL12)
