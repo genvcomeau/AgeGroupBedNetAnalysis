64324e6c7f5b3d7a1d3b1330790fb2869405b3f3
genvcomeau <genvcomeau@gmail.com>
2016-07-28 14:54:08 -0700

    7/28 commit 1
    model finalized, optimized
    starting predicted probabilities for ggplot

--- AgeGroupMasterScript.R
+++ AgeGroupMasterScript.R
@@ -42,3 +42,20 @@ logit.sig.Hi<-
 logit.sig.Lo<-
   glm(LNNET ~ Sex + NETDF + Netspp, 
       data = mlm.Lo, family = "binomial", na.action= na.exclude)
+#assessment of model fit
+with(logit.sig.Hi, null.deviance - deviance)
+with(logit.sig.Hi, df.null - df.residual)
+with(logit.sig.Hi, pchisq(null.deviance - deviance, 
+                df.null - df.residual, lower.tail = FALSE))
+#OR and CIs
+confint.default(logit.sig.Hi)
+exp(cbind(OR = coef(logit.sig.Hi), confint(logit.sig.Hi)))
+#Predicted probabilities by age
+predicted.Hi.F<- with(mlm.Hi,
+     data.frame(Age = factor(1:98), Sex = factor("F"), NETDF = mean(NETDF), SLPSTRUCT = mean(SLPSTRUCT), HHMNo.y = mean(HHMNo.y), Netspp.y = mean(Netspp.y)))
+predicted.Hi<- rbind(predicted.Hi.F, predicted.Hi.M)
+predicted.Lo.M<- with(mlm.Lo,
+                      data.frame(Age = factor(1:98), Sex = "M", Netspp = mean(Netspp, na.rm = TRUE)))
+predicted.Lo.F<- with(mlm.Lo,
+                      +      data.frame(Age = factor(1:98), Sex = "F", Netspp = mean(Netspp, na.rm = TRUE)))
+predicted.Lo <- rbind(predicted.Lo.F, predicted.Lo.M)
